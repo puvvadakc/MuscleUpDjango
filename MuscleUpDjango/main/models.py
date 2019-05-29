dc373aae81a1e2a3217cf62804d9a3fee84f801c
Puvvada <Krishna.Puvvada1@T-Mobile.com>
2019-05-29 15:13:46 -0700

    Added progress functionality

--- MuscleUpDjango/main/models.py
+++ MuscleUpDjango/main/models.py
@@ -33,3 +33,54 @@ class Programs(models.Model):
     fitness_goal = models.CharField(max_length=1, choices=FITNESS_GOAL_TYPE, null = False)
     author = models.ForeignKey(Users, on_delete = models.CASCADE)
     date = models.DateTimeField(auto_now_add= True)
+
+
+
+class Progress(models.Model):
+    user = models.ForeignKey(Users, on_delete = models.CASCADE)
+    program = models.ForeignKey(Programs, on_delete = models.CASCADE)
+    date = models.DateTimeField(auto_now_add= True)
+    height = models.PositiveSmallIntegerField(validators=[MinValueValidator(1), MaxValueValidator(120)], null = False)
+    gender = models.PositiveSmallIntegerField(
+        validators=[MinValueValidator(0), 
+        MaxValueValidator(1)], null = False)
+    age = models.PositiveSmallIntegerField(
+        validators=[MinValueValidator(1), 
+        MaxValueValidator(130)], null = False)
+    barbell_row = models.PositiveSmallIntegerField(
+        validators=[MinValueValidator(1), 
+        MaxValueValidator(1000)], null = True)
+    bench_press = models.PositiveSmallIntegerField(
+        validators=[MinValueValidator(1), 
+        MaxValueValidator(1500)], null = True)
+    dead_lift = models.PositiveSmallIntegerField(
+        validators=[MinValueValidator(1), 
+        MaxValueValidator(1500)], null = True)
+    overhead_press = models.PositiveSmallIntegerField(
+        validators=[MinValueValidator(1), 
+        MaxValueValidator(1000)], null = True)
+    squat = models.PositiveSmallIntegerField(
+        validators=[MinValueValidator(1), 
+        MaxValueValidator(1500)], null = True)
+    dips_count = models.PositiveSmallIntegerField(
+        validators=[MinValueValidator(1), 
+        MaxValueValidator(2000)], null = True)
+    pullups_count = models.PositiveSmallIntegerField(
+        validators=[MinValueValidator(1), 
+        MaxValueValidator(2000)], null = True)
+    pushups_count = models.PositiveSmallIntegerField(
+        validators=[MinValueValidator(1), 
+        MaxValueValidator(2000)], null = True)
+    mile_time_sec = models.PositiveSmallIntegerField(
+        validators=[MinValueValidator(120), 
+        MaxValueValidator(5000)], null = True)
+    heartrate_bpm = models.PositiveSmallIntegerField(
+        validators=[MinValueValidator(20), 
+        MaxValueValidator(250)], null = True)
+    steps = models.PositiveIntegerField(
+        validators=[MinValueValidator(1), 
+        MaxValueValidator(100000)], null = True)
+    weight = models.PositiveSmallIntegerField(
+        validators=[MinValueValidator(20), 
+        MaxValueValidator(1000)], null = True)
+    bodyfat_perc = models.DecimalField(max_digits=2, decimal_places=2, null=True)
